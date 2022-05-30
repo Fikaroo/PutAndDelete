704a947310b4dca1e95ec71af62872e135945a5f
Fikaroo <fuadbayramv@gmail.com>
2022-05-30 16:45:13 +0400

    finish

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const users = require("./users.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`app litening on port ${port}`);
+});
+
+app.put("/user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userWithId = users.findIndex((user) => user.id === userId);
+
+  if (userWithId !== -1) {
+    const oldUser = users[userWithId];
+    users[userWithId] = { ...oldUser, ...req.body };
+
+    res.json(users[userWithId]);
+  } else {
+    res.status(404).json();
+  }
+});
